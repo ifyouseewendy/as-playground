e8e56af0ba653e4d062b4d82f056452d48ac710b
Di Wen <ifyouseewendy@gmail.com>
2020-04-01 10:17:58 -0400

    Add sample script

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,5 +1,13 @@
-// The entry file of your WebAssembly module.
-
-export function add(a: i32, b: i32): i32 {
-  return a + b;
+@unmanaged
+export class Product {
+  public price: i32;
+  constructor(price: i32) {
+    this.price = price;
+  }
 }
+
+export function run(product: Product): Product {
+  return product
+}
+
+export function shopify_runtime_allocate(size: u32): ArrayBuffer { return new ArrayBuffer(size); }
